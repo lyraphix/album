df999abded17bf31c36b5f5ee0c3a08d8645c023
Mark McGovern <mamcgov@emory.edu>
2024-11-10 17:58:20 -0500

    Styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ImageUpload from './components/ImageUpload';
 import ImageGrid from './components/ImageGrid';
 import Albums from './components/Albums';
+import Search from './components/Search';
 import './App.css';
 
 function App() {
@@ -12,11 +13,21 @@ function App() {
     <Router>
       <div className="App">
         <h1 className="main-header">Album</h1>
-        <Routes>
-          <Route path="/" element={<ImageUpload />} />
-          <Route path="/u/:username" element={<Albums />} />
-          <Route path="/u/:username/:albumname" element={<ImageGrid />} />
-        </Routes>
+        <div className="main-content">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Search />
+                  <ImageUpload />
+                </>
+              }
+            />
+            <Route path="/u/:username" element={<Albums />} />
+            <Route path="/u/:username/:albumname" element={<ImageGrid />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
